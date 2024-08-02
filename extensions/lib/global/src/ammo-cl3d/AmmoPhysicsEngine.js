bfdd511403e408263c1a9de360ebdb67d972a4bc
Watunder <vinschen66@gmail.com>
2024-08-02 23:02:57 +0800

    Fix some errors

--- extensions/lib/global/src/ammo-cl3d/AmmoPhysicsEngine.js
+++ extensions/lib/global/src/ammo-cl3d/AmmoPhysicsEngine.js
@@ -1,5 +1,5 @@
 import * as CL3D from "cl3d";
-import { Ammo } from "../../global";
+import { Ammo } from "../../index";
 
 export class AmmoPhysicsEngine {
     /**
@@ -13,7 +13,7 @@ export class AmmoPhysicsEngine {
     Gravity = 10.0;
 
     /**
-     * @type {CL3D.TriangleSelector} 
+     * @type {CL3D.MetaTriangleSelector} 
      */
     World = null;
 
@@ -22,10 +22,9 @@ export class AmmoPhysicsEngine {
      */
     WorldBody = null;
 
-
     constructor() {
         this._collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
-        this._dispatcher = new Ammo.btCollisionDispatcher(_collisionConfiguration);
+        this._dispatcher = new Ammo.btCollisionDispatcher(this._collisionConfiguration);
         this._broadphase = new Ammo.btDbvtBroadphase();
         this._solver = new Ammo.btSequentialImpulseConstraintSolver();
         this._dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
@@ -35,31 +34,48 @@ export class AmmoPhysicsEngine {
         this._dynamicsWorld.setGravity(Ammo.btVector3(0, this.Gravity, 0));
     }
 
-
-    updateWorld(world) {
+    setWorld(world) {
         this.World = world;
 
+        this.updateWorld();
+    }
+
+    /**
+     * @param {CL3D.Vect3d} vector3d 
+     * @returns 
+     */
+    getBulletVector(vector3d) {
+        return new Ammo.btVector3(vector3d.X, vector3d.Y, vector3d.Z);
+    }
+
+    updateWorld() {
         if (this.WorldBody) {
             this._dynamicsWorld.removeRigidBody(this.WorldBody);
         }
 
-        // TODO
-        let groundShape = null;
+        /**
+         * @type {CL3D.Triangle3d[]}
+         */
+        let triangles = [];
+        this.World.getAllTriangles(null, triangles);
+        let pTriMesh = new Ammo.btTriangleMesh();
+        for (let index = 0; index < triangles.length; ++index) {
+            pTriMesh.addTriangle(this.getBulletVector(triangles[index].pointA),
+                                 this.getBulletVector(triangles[index].pointB),
+                                 this.getBulletVector(triangles[index].pointC));
+        }
 
+        let bvhTriShape = new Ammo.btBvhTriangleMeshShape(pTriMesh, true);
         let groundTransform = new Ammo.btTransform();
         groundTransform.setIdentity();
         groundTransform.setOrigin(Ammo.btVector3(0, 0, 0));
 
         let mass = 0.0;
-        let isDynamic = (mass != 0.0);
         let localInertia = new Ammo.btVector3(0, 0, 0);
-
-        if (isDynamic)
-            groundShape.calculateLocalInertia(mass, localInertia);
 
         let myMotionState = new Ammo.btDefaultMotionState(groundTransform);
 
-        let rbInfo = Ammo.btRigidBodyConstructionInfo(mass, myMotionState, groundShape, localInertia);
+        let rbInfo = Ammo.btRigidBodyConstructionInfo(mass, myMotionState, bvhTriShape, localInertia);
         this.WorldBody = new Ammo.btRigidBody(rbInfo);
 
         this._dynamicsWorld.addRigidBody(this.WorldBody);
